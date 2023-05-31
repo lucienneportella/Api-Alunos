6a7568ff60280b036d46b47ca9454fb98987983d
lucienneportella <giustilucienne@gmail.com>
2023-05-31 07:00:03 -0300

    first commit

---
+++ src/main/java/com/example/demo/model/Responsavel.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Responsavel {
+	
+	private Long id;
+	private String email;
+	private String telefone;
+
+}
